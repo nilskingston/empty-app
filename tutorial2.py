80e7cc9363bf2315928b54fd5fb132beb700809f
nilskingston <nilskingston@hanovernorwichschools.org>
2015-09-14 14:38:57 -0400

    Updated from Brython Server: 9/14/2015 2:38:59 PM

--- tutorial2.py
+++ tutorial2.py
@@ -6,6 +6,16 @@ green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, blue)
+xcoordinates = range(100, 600, 10)
+sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
+
+myredcircle = CircleAsset (5, thinline, red)
+xcoordinates2 = range(100, 600, 10)
+sprites = [Sprite(myredcircle, (x, x + 200)) for x in xcoordinates]
+# Generate a list of sprites that form a line!
+
 
 myapp = App()
 myapp.run()
